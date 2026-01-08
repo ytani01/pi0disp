a5c4b06a73517d598f8d497f06a8240f825148fc
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2026-01-09 02:48:00 +0900

    refactor(image): simplify implementation and align with latest logging style

--- src/pi0disp/commands/image.py
+++ src/pi0disp/commands/image.py
@@ -9,10 +9,12 @@ import time
 import click
 from PIL import Image
 
-from .. import ST7789V, __version__, click_common_opts
+from .. import __version__, click_common_opts, get_logger
 from ..disp.disp_spi import SpiPins
-from ..utils.mylogger import errmsg, get_logger
+from ..disp.st7789v import ST7789V
 from ..utils.utils import ImageProcessor
+
+__log = get_logger(__name__)
 
 
 @click.command()
@@ -47,34 +49,26 @@ def image(ctx, image_path, duration, rst, dc, bl, svg, debug):
     )
     __log.debug("rst=%s, dc=%s, bl=%s", rst, dc, bl)
 
-    cmd_name = ctx.command.name
-    __log.debug("cmd_name=%s", cmd_name)
-
-    __log.info("Attempting to display image: %s", image_path)
-
     try:
         if svg:
-            # png_data = cairosvg.svg2png(url=image_path)
-            # source_image = Image.open(io.BytesIO(png_data))
             svg_file = image_path + ".svg"
             cairosvg.svg2png(url=image_path, write_to=svg_file)
             source_image = Image.open(svg_file)
         else:
             source_image = Image.open(image_path)
-    except FileNotFoundError:
-        __log.error("Error: Image file %a  not found.", image_path)
-        sys.exit(1)
     except Exception as e:
-        __log.error("Error opening image %s: %s", image_path, errmsg(e))
+        __log.error("Error opening image %s: %s", image_path, e)
         sys.exit(1)
 
     processor = ImageProcessor()
 
     try:
-        with ST7789V(pin=SpiPins(rst=rst, dc=dc, bl=bl)) as lcd:
-            __log.info(
-                "Displaying original image resized to screen (contain mode)..."
+        with ST7789V(pin=SpiPins(rst=rst, dc=dc, bl=bl), debug=debug) as lcd:
+            __log.debug(
+                "Display initialized: %sx%s", lcd.size.width, lcd.size.height
             )
+
+            print(f"Displaying image: {image_path}")
 
             # Resize while maintaining aspect ratio
             resized_image = processor.resize_with_aspect_ratio(
@@ -87,18 +81,17 @@ def image(ctx, image_path, duration, rst, dc, bl, svg, debug):
             time.sleep(duration)
 
             for gamma in (1.0, 1.5, 1.0, 0.5, 1.0):
-                __log.info("Applying gamma=%s", gamma)
+                __log.debug("Applying gamma=%s", gamma)
                 corrected_image = processor.apply_gamma(
                     resized_image, gamma=gamma
                 )
                 lcd.display(corrected_image)
                 time.sleep(duration)
 
-    except RuntimeError as e:
-        __log.error(errmsg(e))
-        sys.exit(1)
+    except KeyboardInterrupt:
+        pass
     except Exception as e:
-        __log.error(errmsg(e))
+        __log.error(f"Error occurred: {e}")
         sys.exit(1)
     finally:
-        __log.info("Image display finished.")
+        print("Done.")
